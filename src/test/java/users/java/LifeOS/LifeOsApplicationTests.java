223dc61397d464b0651eb4526277bce7e25680f7
Risspecct <rishikgarg.dev@gmail.com>
2026-04-29 16:33:20 +0530

    initial commit

---
+++ src/test/java/users/java/LifeOS/LifeOsApplicationTests.java
@@ -0,0 +1,13 @@
+package users.java.LifeOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
