8591798ac27caa7e0f3057848ef06de0ca621979
Risspecct <rishikgarg.dev@gmail.com>
2026-05-17 15:50:51 +0530

    added endpoint to fetch details needed only for the dashboard

---
+++ src/main/java/users/java/LifeOS/Dashboard/DashboardController.java
@@ -0,0 +1,21 @@
+package users.java.LifeOS.Dashboard;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    @GetMapping
+    public ResponseEntity<DashboardResponse> getDashboard() {
+
+        return ResponseEntity.ok(
+                dashboardService.getDashboardData()
+        );
+    }
+}
