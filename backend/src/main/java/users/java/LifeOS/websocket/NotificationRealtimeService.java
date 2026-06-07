5f9a99509fb7c98add4c82b50d4fba362a90f0e7
Risspecct <rishikgarg.dev@gmail.com>
2026-06-08 01:07:35 +0530

    feat: friend related notifications are  real time now

--- backend/src/main/java/users/java/LifeOS/websocket/NotificationRealtimeService.java
+++ backend/src/main/java/users/java/LifeOS/websocket/NotificationRealtimeService.java
@@ -3,21 +3,22 @@ package users.java.LifeOS.websocket;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import users.java.LifeOS.notification.NotificationResponse;
+import users.java.LifeOS.user.User;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationRealtimeService {
-    private final SimpMessagingTemplate messagingTemplate;
 
-    public void sendTestNotification(String email) {
-        NotificationRealtimeDto dto = new NotificationRealtimeDto(
-                        "Hello from WebSocket"
-                );
+    private final SimpMessagingTemplate
+            messagingTemplate;
+
+    public void publish(User recipient, NotificationResponse dto) {
 
         messagingTemplate.convertAndSendToUser(
-                email,
-                "/queue/notifications",
-                dto
-        );
+                        recipient.getEmail(),
+                        "/queue/notifications",
+                        dto
+                );
     }
 }
